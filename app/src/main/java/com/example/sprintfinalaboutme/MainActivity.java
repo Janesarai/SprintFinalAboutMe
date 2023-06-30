490bcf66acded47eed0582ba00aa1fa9087efa36
Janesarai <saraipalaciosjane@gmail.com>
2023-06-29 20:31:59 -0600

    1 y 2 pantalla funcionando (falta arreglar diseño)

--- app/src/main/java/com/example/sprintfinalaboutme/MainActivity.java
+++ app/src/main/java/com/example/sprintfinalaboutme/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.sprintfinalaboutme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.sprintfinalaboutme.databinding.ActivityMainBinding;
 
@@ -13,5 +15,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        binding.buttonCompartir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent activityMenu= new Intent(getBaseContext(), SecondActivity.class);
+                startActivity(activityMenu);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/sprintfinalaboutme/SecondActivity.java
+++ app/src/main/java/com/example/sprintfinalaboutme/SecondActivity.java
@@ -2,7 +2,10 @@ package com.example.sprintfinalaboutme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.sprintfinalaboutme.databinding.ActivitySecondBinding;
 
@@ -14,5 +17,29 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding= ActivitySecondBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        binding.linkedin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent alinkedin = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.linkedin.com/in/gencina2/"));
+                startActivity(alinkedin);
+            }
+        });
+
+        binding.wasap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent aWasap = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.whatsapp.com/?lang=es"));
+                startActivity(aWasap);
+            }
+        });
+
+        binding.correo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent aCorreo = new Intent(Intent.ACTION_VIEW, Uri.parse("https://accounts.google.com/AccountChooser/signinchooser?service=mail&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&flowName=GlifWebSignIn&flowEntry=AccountChooser"));
+                startActivity(aCorreo);
+            }
+        });
     }
 }
